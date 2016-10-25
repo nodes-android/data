bbdafcecf04699c8f3eb8cb2a1a74506f28b0e1e
Johnny Sørensen <joso@nodes.dk>
2016-10-25 16:34:51 +0200

    trying to deploy again [ci deploy]

--- exampleproject/src/main/java/dk/nodes/exampleproject/MainActivity.java
+++ exampleproject/src/main/java/dk/nodes/exampleproject/MainActivity.java
@@ -2,14 +2,26 @@ package dk.nodes.exampleproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+
         UserDataManager userDataManager = new UserDataManager(this.getApplicationContext());
         UserData userData = userDataManager.load();
+        userData.userName = "test";
+        userDataManager.save(userData);
+
+        userData = userDataManager.load();
+        textView.setText(userData.userName);
     }
 }
--- exampleproject/src/main/java/dk/nodes/exampleproject/UserData.java
+++ exampleproject/src/main/java/dk/nodes/exampleproject/UserData.java
@@ -1,5 +1,7 @@
 package dk.nodes.exampleproject;
 
+import java.io.Serializable;
+
 import dk.nodes.data.annotation.Data;
 import dk.nodes.data.annotation.Mode;
 import dk.nodes.data.annotation.Persistence;
@@ -8,10 +10,10 @@ import dk.nodes.data.annotation.Persistence;
  * Created by joso on 30/05/16.
  */
 @Data(
-        persistence = Persistence.GSON,
-        mode = Mode.SHAREDPREFERENCES
+        persistence = Persistence.SERIALIZATION,
+        mode = Mode.FILE
 )
-public class UserData {
+public class UserData implements Serializable {
     public String token = "";
     public String userName = "";
 }
